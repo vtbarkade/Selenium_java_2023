49b474afa89b1e410b940ba4619a02d27985ae07
Vijay <barkade.vijay1989@gmail.com>
2024-03-15 09:54:46 +0530

    Third commit

---
+++ src/test/java/Practice_Java/Practice_18_Toggle_Case_AgTP_to_aGtp.java
@@ -0,0 +1,30 @@
+package Practice_Java;
+
+public class Practice_18_Toggle_Case_AgTP_to_aGtp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "HbTpMshT";
+		String res ="";
+		int leng= str.length();
+		
+		for(int i=0;i<leng;i++)
+		{
+			char c= str.charAt(i);
+			
+			if(c>='a'&&c<='z') // is lowercase convert to uppercase
+			{
+				res = res +(char)((int)c+((int)'A'-(int)'a'));
+			}
+			if(c>='A'&&c<='Z') // is uppercase convert to lowercase
+			{
+				res = res +(char)((int)c-((int)'A'-(int)'a'));
+			}
+		}
+		
+		System.out.println("Given data: "+str+"\nOutput data : "+res);
+						
+	}
+
+}
